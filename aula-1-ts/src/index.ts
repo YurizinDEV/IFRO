e3b9becc089bd6c5853fcf9a52713609d886aaed
Yuri Zetoles <132094461+YurizinDEV@users.noreply.github.com>
2024-12-02 22:19:00 -0400

    Add files via upload

---
+++ aula-1-ts/src/index.ts
@@ -0,0 +1,33 @@
+/*let idade: number = 20;
+let nome: string = "Maria";
+let pagamentoEfetuado: boolean = false;
+
+
+idade = 18;
+nome = "Maria da Silva";
+pagamentoEfetuado = true;
+
+//console.log(`Nome: ${nome}`);
+//console.log(`Idade: ${idade}`);
+
+//console.log(`Nome: ${nome} tem ${idade} ano(s)`);
+
+// Enum*/
+/*enum Direcao{
+    Norte = "NORTE",
+    Sul = "SUL",
+    Leste = "LESTE",
+    Oeste = "OESTE"
+};
+let direcaoTomada: Direcao = Direcao.Sul;
+console.log(`Direção tomda: ${direcaoTomada}`);*/
+
+/*enum Cor{
+    Red   = "#FF0000",
+    Green = "#00FF00",
+    Blue  = "#0000FF"
+}
+let corEscolhida: Cor = Cor.Red;
+console.log(`Cor escolhida: ${corEscolhida}`);*/
+//
+

504a17b87bd758e331c78ae4a83e256005b66335
YurizinDEV <yurizetoles0123@gmail.com>
2025-02-05 19:16:46 -0400

    Novo

---
+++ LinguagensProgramação/2025-02-05-ts /src/utils/validacoes.ts
@@ -0,0 +1,15 @@
+import z from "zod";
+
+export function validarNome(nome: string): boolean {
+    return nome.length >= 3 && nome.length <= 25;
+}
+
+export function validarEmail(email: string): boolean {
+    const regexEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return regexEmail.test(email);
+}
+
+export function validarSenha(senha: string): boolean {
+    const regexSenha = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{8,}$/;
+    return regexSenha.test(senha);
+}
